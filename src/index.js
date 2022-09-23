184af6b6184870ad135426fd17afd9fed7bd0596
aqdasilva <antqdasilva@gmail.com>
2022-09-23 08:28:04 -0500

    using setState to get the users latitude location

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { render } from "@testing-library/react";
+import React from "react";
+import  ReactDOM  from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+class App extends React.Component{
+constructor(props) {
+    super(props);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    //only time we do direct assignment to this.state
+    this.state = {lat: null};
+
+    window.navigator.geolocation.getCurrentPosition(
+        (position) => {
+            //we called set state here to update
+            this.setState({lat: position.coords.latitude });
+        },
+        (err) => console.log(err)
+    );
+}
+
+
+    //React says we have to render !!!!!!
+    render() {
+        return <div>Latitude: {this.state.lat}</div>;
+    }
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
