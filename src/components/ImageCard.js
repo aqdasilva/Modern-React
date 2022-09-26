4b9809cbc93023188cf4e5a12b63a9ebafdc09aa
aqdasilva <antqdasilva@gmail.com>
2022-09-26 14:01:24 -0500

    spaced out imgs evenly

---
+++ src/components/ImageCard.js
@@ -0,0 +1,27 @@
+//to show one image by its self
+
+import React from "react";
+
+class ImageCard extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.imageRef = React.createRef();
+    }
+
+
+    render() {
+        const {description, urls } = this.props.image;
+
+
+        return (
+            <div>
+                <img ref={this.imageRef}
+                alt={description} 
+                src={urls.regular}/>
+            </div>
+        )
+    }
+}
+
+export default ImageCard;
