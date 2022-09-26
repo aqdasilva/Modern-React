d107cf56406ce8186c1a60d7156bfd4f4d8b4911
aqdasilva <antqdasilva@gmail.com>
2022-09-26 11:55:35 -0500

    added async

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,29 @@
+import { render } from "@testing-library/react";
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
-const App = () => {
-    return (
-        <div className="ui container" style={{marginTop: '10px'}}>
-            <SearchBar />
-        </div>
-    );
-};
+
+class App extends React.Component {
+    async onSearchSubmit(term) {
+        const response = await axios.get('https://api.unsplash.com/search/photos', {
+            params: {query: term},
+            headers: {
+                Authorization: 'Client-ID 2Ygwtrbjfd9kSnrrwsLPZykFGV4H9cJO2slPkktvhPY'
+            }
+        });
+
+        console.log(response.data.results);
+    }
+
+
+    render() {
+        return (
+            <div className="ui container" style={{marginTop: '10px'}}>
+                <SearchBar onSubmit={this.onSearchSubmit} />
+            </div>
+        );
+    }
+}
 
 export default App;
